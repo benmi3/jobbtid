bc1ad05c82ff64e83109cecae5d9f7279dabb79b
Benjamin Jørgensen <me@benmi.me>
2025-04-18 22:41:25 +0900

    server hmmm

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,17 @@
 /*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2025 Benjamin Jørgensen <me@benmi.me>
 */
 package cmd
 
 import (
+	"fmt"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -25,7 +26,13 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("serve called")
+		serve()
+	},
 }
+
+var cfgFilePath string
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
@@ -41,11 +48,55 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.jobbtid.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFilePath, "config", "", "config file (default is $HOME/.jobbtid.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World! %s", time.Now())
+}
 
+type serverConfig struct {
+	Host string
+}
+
+func (conf serverConfig) host() string {
+	return conf.Host
+}
+
+func readConfigFile() (serverConfig, error) {
+	myServerConfig := serverConfig{
+		Host: ":8080",
+	}
+	viper.AddConfigPath(cfgFilePath)
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; ignore error if desired
+			fmt.Println("Using default!")
+			return myServerConfig, nil
+		} else {
+			// Config file was found but another error was produced
+		}
+	} else {
+		host := viper.GetString("host")
+		if len(host) == 0 {
+			host = ":8080"
+			myServerConfig.Host = host
+		}
+	}
+	return myServerConfig, err
+}
+
+func serve() {
+	myConf, err := readConfigFile()
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	http.HandleFunc("/", greet)
+	http.ListenAndServe(myConf.host(), nil)
+}
--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 /*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2025 Benjamin Jørgensen <me@benmi.me>
 */
 package main
 
