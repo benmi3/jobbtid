78b953da8da70ba5e5c1fa4b9030af3ba8bd9655
Benjamin Jørgensen <me@benmi.me>
2025-05-02 21:12:33 +0900

    the test should succeed now, checking how sonarqube will get the results

--- pkg/db/db_test.go
+++ pkg/db/db_test.go
@@ -9,8 +9,8 @@ import (
 
 func TestGetDefaultConfig(t *testing.T) {
 	t.Run("Can Create Default Config", func(t *testing.T) {
-		res := createDefaultConfig()
-		if res.ServerConf.Default != true {
+		_, err := setupDbCon()
+		if err != nil {
 			t.Error("Wanted default is true, got false")
 		}
 	})
