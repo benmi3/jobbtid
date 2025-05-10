5771fe9f07b50e5b2a0d85568b388270646c6b72
Benjamin Jørgensen <me@benmi.me>
2025-05-10 22:48:48 +0900

    more tests and 1 test config toml file. that will be moved later

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -15,3 +15,57 @@ func TestGetDefaultConfig(t *testing.T) {
 		}
 	})
 }
+
+func TestGetDefaultConfigHost(t *testing.T) {
+	t.Run("Can get host when configName is set to server", func(t *testing.T) {
+		res := createDefaultConfig()
+		if res.ServerConf.Default != true {
+			t.Error("Wanted default is true, got false")
+		}
+		host_test, err := res.Host("server")
+		if err != nil {
+			t.Error("host_test returned an error")
+		}
+		default_host := ":8080"
+		if host_test != default_host {
+			t.Errorf("host_test name returned %q, wanted %q", host_test, default_host)
+		}
+	})
+}
+
+func TestGetDefaultConfigHostError(t *testing.T) {
+	t.Run("Host will error if unsupported configName is set", func(t *testing.T) {
+		res := createDefaultConfig()
+		if res.ServerConf.Default != true {
+			t.Error("Wanted default is true, got false")
+		}
+		host_test, err := res.Host("")
+		if err == nil {
+			t.Error("host_test returned nil instead of error")
+		}
+		default_host := ""
+		if host_test != default_host {
+			t.Errorf("host_test name returned %q, wanted %q", host_test, default_host)
+		}
+	})
+}
+
+func TestReadConfigFile(t *testing.T) {
+	t.Run("Can Read from config", func(t *testing.T) {
+		res, err := ReadConfigFile("./config_test.toml")
+		if res.ServerConf.Default != false {
+			t.Error("Wanted default is false, got true")
+		}
+		if err != nil {
+			t.Error("Error happened when reading empty config file. should not happen")
+		}
+		host_test, err := res.Host("server")
+		if err != nil {
+			t.Error("host_test returned error instead of nil")
+		}
+		want_host := "localhost:8081"
+		if host_test != want_host {
+			t.Errorf("host_test name returned %q, wanted %q", host_test, want_host)
+		}
+	})
+}
--- pkg/db/db_test.go
+++ pkg/db/db_test.go
@@ -4,14 +4,49 @@ Copyright © 2025 Benjamin Jørgensen <me@benmi.me>
 package db
 
 import (
+	"encoding/json"
 	"testing"
 )
 
-func TestGetDefaultConfig(t *testing.T) {
-	t.Run("Can Create Default Config", func(t *testing.T) {
+func TestDefaultDbSetup(t *testing.T) {
+	t.Run("Debug Setup", func(t *testing.T) {
 		_, err := setupDbCon()
 		if err != nil {
 			t.Error("Wanted default is true, got false")
+		}
+	})
+}
+
+func TestDefaultDbRun1(t *testing.T) {
+	t.Run("One run db", func(t *testing.T) {
+		uid := "testUser"
+		jobbdag := "2025/5/10"
+		starttime := "2025/5/10 10:10"
+		stoptime := "2025/5/10 10:11"
+		id, err := Create(uid, jobbdag, starttime, stoptime)
+		if err != nil {
+			t.Error("error happended during creation")
+		}
+		u_id, err := Update(id, uid, jobbdag, starttime, stoptime)
+		if err != nil {
+			t.Errorf("error happended during updating errror %q", err)
+		}
+		if u_id != id {
+			t.Error("update id is not the same as create id")
+		}
+		testItem, err := GetByDate(uid, jobbdag)
+		if err == nil {
+			var myTestStruct Jobbtid
+			marErr := json.Unmarshal(testItem, &myTestStruct)
+			if marErr != nil {
+				t.Error("failed at parsig GetByDate data")
+			}
+			if myTestStruct.Id != id {
+				t.Errorf("myTestStruct.Id(%d) did not match id(%d) and update_id(%d)", myTestStruct.Id, id, u_id)
+			}
+
+		} else {
+			t.Error("error occured when executing GetByDate")
 		}
 	})
 }
